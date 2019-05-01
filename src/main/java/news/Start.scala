90feef52d11a567a761ad78b082af4f4089aef0e
Oleg Vaiser <aly9681082@gmail.com>
2019-05-01 10:22:04 +0300

    correct

--- src/main/java/news/Start.scala
+++ src/main/java/news/Start.scala
@@ -12,7 +12,7 @@ object Start {
   private val AUTHOR = "author"
   private val TOPIC = "topic"
   private val WORDS = "words"
-  private val POLITIC_WORDS = "politic_words"
+
 
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setMaster("local[*]").setAppName("news_scala")
@@ -20,25 +20,20 @@ object Start {
     val session = SparkSession.builder.getOrCreate
     val dataString1 = session.read.json("data/news/news.json")
     dataString1.show()
-    val dataString2 = dataString1.withColumn(POLITIC_WORDS, col(WORDS).multiply(WORDS))
-    dataString2.show()
+
 
     val politic_words = List("human rights", "Democrats")
 
 
     val g: String => String = _.filter(_.equals(politic_words))
     val s = udf(g)
-    dataString2.withColumn("pol",s(col(WORDS))).show()
+    dataString1.withColumn("pol",s(col(WORDS))).show()
   }
 }
 
-/*val salaries: List[Int] = List(10, 20, 30, 40, 50);*/
 
 
 
 
 
-/*val upper: String => String = _.toUpperCase
-    val s = udf(upper)
-    session.udf.register("myUpper", (input: String) => input.toUpperCase)
-    dataString2.withColumn("upper", s(col(WORDS))).show()*/
+
